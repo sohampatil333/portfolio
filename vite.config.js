7aa1927f5144990ef04a3a7fc0e7869b9f3401bc
Soham Patil <patilsoham0303@gmail.com>
2026-01-17 11:40:59 +0530

    Updated portfolio UI and content

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // <--- ADD THIS LINE (Must match your repo name)
+  base: '/portfolio/',   // 👈 repo name
 })
